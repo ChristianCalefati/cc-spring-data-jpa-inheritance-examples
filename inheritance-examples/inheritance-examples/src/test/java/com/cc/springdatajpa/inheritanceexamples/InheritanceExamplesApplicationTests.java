337157ce4ac9358ea8401aa2b4ddfbfcf00daf22
Christian Calefati <71175246+ChristianCalefati@users.noreply.github.com>
2022-02-13 18:44:15 +0100

    Initial Commit

---
+++ inheritance-examples/inheritance-examples/src/test/java/com/cc/springdatajpa/inheritanceexamples/InheritanceExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cc.springdatajpa.inheritanceexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InheritanceExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
